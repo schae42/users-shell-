4e6e8a231a30e5fa08264cb2d3ebd205e66b4b8a
schae42 <chaechan@outlook.com>
2020-10-06 22:03:47 -0700

    users-with-templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Users
 # Create your views here.
+def index(request):
+    for users in Users.objects.all():
+        print(users.__dict__)
+    context = {
+        'all_users': Users.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def process(request):
+    Users.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email_address=request.POST['email_address'],
+        age=request.POST['age']
+        )
+    return redirect('/')
